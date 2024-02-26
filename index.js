0e59a9046857d1c5a392efe17fa7ea2ee327eee6
Xavier Humes-Sanchez <xfactorx109@gmail.com>
2024-02-25 18:13:45 -0800

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(place = "go to the office") {
+    return `This Monday, I will ${place}.`;
+}
+
+function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+        return `You are ${highlight}${adjective}${highlight}!`;
+    };
+}
